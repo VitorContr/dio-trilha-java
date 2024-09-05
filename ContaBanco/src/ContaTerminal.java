7bea3d23b9aa4496e9ab6041fb1a5fd3d1a4873e
Vitor Contreiras <vitorcontr@gmail.com>
2024-09-05 16:48:22 -0300

    Primeiro commit conta banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,15 @@
+
+/* 
+
+Numero	    Inteiro	        1021
+Agencia	    Texto	        067-8
+Nome	    Texto	        MARIO ANDRADE
+Saldo	    Decimal	        237.48
+
+*/
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
