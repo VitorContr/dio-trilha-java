49d7a604e052653a344e60584f6ee366824203b6
Vitor Contreiras <vitorcontr@gmail.com>
2024-09-05 17:40:24 -0300

    Segundo commit conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /* 
 
@@ -9,7 +10,38 @@ Saldo	    Decimal	        237.48
 */
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public String nome; 
+    public int numero;
+    public String agencia;
+    public Double saldo;
+
+
+
+    private ContaTerminal(String nome, int numero, String agencia, Double saldo) {
+       
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nome = nome; 
+        this.saldo = saldo;
+    }
+
+    public static void main(String[] args) throws Exception { 
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        int numero = sc.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = sc.next(); 
+
+        System.out.println("Por favor, digite seu Nome: ");
+        String nome = sc.next(); 
+
+        System.out.println("Por favor, digite o Saldo: ");
+        Double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nome + ", Obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + numero + " e seu saldo: " + saldo + " já está disponível para saque !");
     }
 }
